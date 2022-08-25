70bc1e254fcd26063a20731749c01568c9fe24fa
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-08-25 17:47:53 +0200

    fixed Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,42 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
-startBtn.disabled = false;
-stopBtn.disabled = true;
 
 startBtn.addEventListener('click', onStartBtnClick);
 stopBtn.addEventListener('click', onStopBtnClick);
 
-let intervalID;
-function onStartBtnClick() {
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
-  intervalID = setInterval(() => {
-    const bodyColor = getRandomHexColor();
+isActiveStartPositionBtns();
 
-    document.body.style.backgroundColor = bodyColor;
+let intervalID;
+
+function onStartBtnClick() {
+  changeActiveOfBtns();
+  changeBodyColor();
+  intervalID = setInterval(() => {
+    changeBodyColor();
   }, 1000);
 }
 
 function onStopBtnClick() {
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
+  isActiveStartPositionBtns();
   clearInterval(intervalID);
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function isActiveStartPositionBtns() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+function changeActiveOfBtns() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function changeBodyColor() {
+  const bodyColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = bodyColor;
 }
