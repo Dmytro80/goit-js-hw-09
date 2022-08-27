53c7c2e913262121b2c090592b7eb674970ea7a5
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-08-27 15:37:08 +0200

    Task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,6 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const form = document.querySelector('.form');
-const amountRef = document.querySelector('[name="amount"]');
 
 form.addEventListener('submit', onSubmitForm);
 
@@ -21,9 +22,11 @@ function onSubmitForm(event) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
 
     position += 1;
